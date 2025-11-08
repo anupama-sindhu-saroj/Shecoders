d006bd5c8975de28f585b440daa77644f41bb06b
Anushka Wanve <anushkawanve28@gmail.com>
2025-11-08 08:19:04 +0530

    addons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import AuthContainer from "./components/AuthContainer/AuthContainer";
 import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
 import Dashboard from "./pages/Dashboard"; // ✅ Added import
+import LandingPage from './pages/LandingPage';
+
 
 import CreateQuiz from "./pages/CreateQuiz";
 import PreviewQuiz from "./pages/PreviewQuiz";
