dd86bbbf9c6534ca959c33a96dc692d40f7b52ef
anupama-sindhu-saroj <anupamaixb04@gmail.com>
2025-11-08 06:27:21 +0530

    landing path added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import AuthContainer from "./components/AuthContainer/AuthContainer";
 import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
-<<<<<<< HEAD
 import Dashboard from "./pages/Dashboard"; 
-=======
 import LandingPage from './pages/LandingPage'
->>>>>>> c854624 (landing page)
 import "./index.css";
 
 function App() {
@@ -16,17 +13,12 @@ function App() {
     <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
       <Router>
         <Routes>
-<<<<<<< HEAD
 
-          <Route path="/" element={<AuthContainer />} />
-
- 
-=======
-          <Route path="/" element={<LandingPage />} />
-
+        <Route path="/" element={<LandingPage />} />
+          {/* Main login/signup page */}
           <Route path="/auth" element={<AuthContainer />} />
 
->>>>>>> c854624 (landing page)
+ 
           <Route path="/forgot-password" element={<ForgotPassword />} />
 
           <Route path="/reset-password/:token" element={<ResetPassword />} />
